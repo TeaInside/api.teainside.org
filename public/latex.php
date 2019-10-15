2e373737ad11e338d17272a640afb56070372c27
Ammar Faizi <ammarfaizi2@gmail.com>
2019-10-15 22:46:26 +0700

    Update api

--- public/latex.php
+++ public/latex.php
@@ -7,8 +7,9 @@ use Gregwar\Tex2png\Tex2png;
 if (isset($_GET["exp"]) && is_string($_GET["exp"])) {
 	header("Content-Type: application/json");
 	$hash = sha1($_GET["exp"]);
-	Tex2png::create($_GET["exp"], "600x600")
-	    ->saveTo(__DIR__."/latex/{$hash}.png")
+	$st = Tex2png::create($_GET["exp"], "600x600");
+	$st->packages[] = "margin=1in";
+	$st->saveTo(__DIR__."/latex/{$hash}.png")
 	    ->generate();
 
 	print json_encode(
