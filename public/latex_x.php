ce87a42d49b47cc8a7041523fc5798ae7724063b
Ammar Faizi <ammarfaizi2@gmail.com>
2019-11-25 20:56:00 +0700

    Update latex_x.php

--- public/latex_x.php
+++ public/latex_x.php
@@ -11,7 +11,7 @@ if (isset($_GET["exp"]) && is_string($_GET["exp"])) {
 
 	if (strpos($expr, "\\int") !== false) {
 		$expr = str_replace("\\int ", "\\int\\(pure_space)", $expr);
-		$expr = implode("\\;d", explode("d", $expr));
+		$expr = implode("\\;d", preg_split("/(?<=[^\\\\])d/", $expr));
 	}
 
 	$expr = str_replace(" ", "\\,", trim($expr));
