75693f0d818beab945cf77a96b5e53dba2bfc19e
Ammar Faizi <ammarfaizi2@gmail.com>
2020-03-09 19:40:52 +0700

    Add corona API

---
+++ public/corona/index.php
@@ -0,0 +1,25 @@
+<?php
+
+define("CORONA_STATS_STORAGE", __DIR__."/data");
+
+require __DIR__."/CoronaStatistic.php";
+
+$st = new CoronaStatistic;
+$now = strtotime(gmdate("Y-m-d H:i:s"));
+
+if (isset($_GET["country"])) {
+    $data = $st->getCountry($_GET["country"]);
+    if (abs($now - $data["scraped_at"]) > 3600) {
+        $st->scrape();
+        $data = $st->getCountry($_GET["country"]);
+    }
+} else {
+    $data = $st->getGlobal();
+    if (abs($now - $data["scraped_at"]) > 3600) {
+        $st->scrape();
+        $data = $st->getGlobal();
+    }
+}
+
+header("Content-Type: application/json");
+echo json_encode($data);
