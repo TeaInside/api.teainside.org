6cf9860552f8bc86ed5689b559c4df14c1a81107
Ammar Faizi <ammarfaizi2@gmail.com>
2020-03-09 19:53:13 +0700

    Add corona API

--- public/corona/index.php
+++ public/corona/index.php
@@ -9,13 +9,13 @@ $now = strtotime(gmdate("Y-m-d H:i:s"));
 
 if (isset($_GET["country"])) {
     $data = $st->getCountry($_GET["country"]);
-    if (abs($now - $data["scraped_at"]) > 3600) {
+    if ((!isset($data["scraped_at"])) || (abs($now - $data["scraped_at"]) > 3600)) {
         $st->scrape();
         $data = $st->getCountry($_GET["country"]);
     }
 } else {
     $data = $st->getGlobal();
-    if (isset($data["scraped_at"]) && (abs($now - $data["scraped_at"]) > 3600)) {
+    if ((!isset($data["scraped_at"])) || (abs($now - $data["scraped_at"]) > 3600)) {
         $st->scrape();
         $data = $st->getGlobal();
     }
